9641e7e73ec2acb33f0f074499858288ab013675
Bulgak Viktoriya <bulgak.viktoriya@gmail.com>
2018-04-09 06:46:39 +0300

    change homework5 task4

--- homework5/task4.js
+++ homework5/task4.js
@@ -1,26 +1,81 @@
-var array=new Array(12);
-array.fill(0);
-var countries=new Array('Россия','Беларусь','Польша','Литва','Латвия','Украина',
-	'Германия','Великобритания','Чехия','Болгария','Франция','Италия');
-var flags=new Array('img/1.jpg','img/2.jpg','img/3.jpg','img/4.jpg',
-	'img/5.jpg','img/6.jpg','img/7.jpg','img/8.jpg','img/9.jpg','img/10.jpg',
-	'img/11.jpg','img/12.jpg');
-var population=new Array('146 880 432','9 491 800','37 967 209','2 888 558','1 968 957',
-	'42 650 186','82 175 684','65 341 183','10 553 843','7 153 784','64 513 242','60 665 55');
-var cods=new Array('7','375','48','370','371','380','49','44','420','359','33','39');
-for(i in array){
-	array[i]={
-		country: countries[i],
-		flag: flags[i],
-		cod: cods[i],
-		countPeople: population[i]
-		}
+var array=new Array();
+array[0]={
+	country: 'Россия',
+	flag: 'img/1.jpg',
+	cod: '7',
+	countPeople: '146 880 432'
 	}
-console.log(array);
+array[1]={
+	country: 'Беларусь',
+	flag: 'img/2.jpg',
+	cod: '375',
+	countPeople: '9 491 800'
+	}
+array[2]={
+	country: 'Польша',
+	flag: 'img/3.jpg',
+	cod: '48',
+	countPeople: '37 967 209'
+	}
+array[3]={
+	country: 'Литва',
+	flag: 'img/4.jpg',
+	cod: '370',
+	countPeople: '2 888 558'
+	}
+array[4]={
+	country: 'Латвия',
+	flag: 'img/5.jpg',
+	cod: '371',
+	countPeople: '1 968 957'
+	}
+array[5]={
+	country: 'Украина',
+	flag: 'img/6.jpg',
+	cod: '380',
+	countPeople: '42 650 186'
+	}	
+array[6]={
+	country: 'Германия',
+	flag: 'img/7.jpg',
+	cod: '49',
+	countPeople: '82 175 684'
+	}
+array[7]={
+	country: 'Великобритания',
+	flag: 'img/8.jpg',
+	cod: '44',
+	countPeople: '65 341 183'
+	}
+array[8]={
+	country: 'Чехия',
+	flag: 'img/9.jpg',
+	cod: '420',
+	countPeople: '10 553 843'
+	}
+array[9]={
+	country: 'Болгария',
+	flag: 'img/10.jpg',
+	cod: '359',
+	countPeople: '7 153 784'
+	}	
+array[10]={
+	country: 'Франция',
+	flag: 'img/11.jpg',
+	cod: '33',
+	countPeople: '64 513 242'
+	}
+array[11]={
+	country: 'Италия',
+	flag: 'img/12.jpg',
+	cod: '39',
+	countPeople: '60 665 55'
+	}
+
 var table=document.createElement("table");
 document.body.appendChild(table);
 table.innerHTML="<tr><td>Страна</td><td>Флаг</td><td>Население</td><td>Телефонный код</td></tr>";
-for(var i=0; i<array.length;i++){
+for(var i=0; i<array.length; i++){
 	var tr=document.createElement("tr");
 	table.appendChild(tr);
 	
