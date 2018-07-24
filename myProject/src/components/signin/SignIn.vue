2b76df3daf930e1315b804913b1a1d87adf81863
Nikita Laptev <nikita.laptev98@inbox.ru>
2018-07-24 12:07:00 +0300

    change logic for authentication form

--- myProject/src/components/signin/SignIn.vue
+++ myProject/src/components/signin/SignIn.vue
@@ -9,72 +9,54 @@
 	            <Login @sendLogin="myLog=$event"></Login>
 	            <Password @sendPassword="myPassw=$event"></Password>
 	        </div>
-	       <button class="signIn" type="signIn" @click="conect"><router-link tag="a" to="/WorkSpace/Reports">Sign In</router-link></button>
+	       <button class="signIn" type="signIn" @click="connect">Sign In</button>
 	    </div>
 	</div>
 </div>
 </template>
 
 <script>
-	import Login from '../input/Login.vue';
-	import Password from '../input/Password.vue';
-	import { bus } from '../../eventBus.js';
-	
-	export default {
-		data() {
-			return {
-				myLog:'',
-				myPassw:'',
-				flag: false
-			}
-		},
-		components: {
-			Login,
-			Password
-		},
-		methods: {
-			getUser() {
-				const xhr = new XMLHttpRequest();
-				xhr.open('GET', this.$root.URL+'/auth'); 
-				xhr.setRequestHeader( "Content-Type", "application/x-www-form-urlencoded" ); 
-				xhr.withCredentials = true; 
-				xhr.send(null); 
-				xhr.onload = function() {
-					let user = JSON.parse(xhr.responseText);
-					console.log(user.role);
-					if(user.role == "admin") {
-						this.flag = true;
-					}
-					bus.$emit('doFlag', this.flag);
-				}
-			},
-			conect() {
-				if(this.myLog.match(/^[a-zA-Z]+[a-zA-Z0-9\-]{6,20}$/)&&this.myPassw.match(/^[a-zA-Z0-9]{7,20}$/)) {
-					const xhr = new XMLHttpRequest();
-					xhr.open('POST', this.$root.URL+'/auth'); 
-					xhr.setRequestHeader( "Content-Type", "application/x-www-form-urlencoded" ); 
-					xhr.withCredentials = true; 
-					xhr.send(`Login=${this.myLog}&Password=${this.myPassw}`); 
-					xhr.onload=function() {
-						console.log(xhr.responseText);
-					}
-				} else {
-					alert("Ошибка при вводе данных!");
-					if(!this.myLog.match(/^[a-zA-Z]+[a-zA-Z0-9\-]{6,20}$/)) {
-						console.log(e.target.parentNode.children[1].children[0]);
-					}
-					if(!this.myPassw.match(/^[a-zA-Z0-9]{7,20}$/)) {
-						console.log(e.target.parentNode.children[1].children[1]);
-					}
-					if(!this.myLog.match(/^[a-zA-Z]+[a-zA-Z0-9\-]{6,20}$/)&&!this.myPassw.match(/^[a-zA-Z0-9]{7,20}$/)) {
-						console.log(e.target.parentNode.children[1].children[0]);
-							console.log(e.target.parentNode.children[1].children[1]);
-					}
-				}
-				
-			}
-		}
-	}
+import Login from "../input/Login.vue";
+import Password from "../input/Password.vue";
+import { bus } from "../../eventBus.js";
+import request from "../../request.js";
+import swal from "sweetalert2";
+
+const map = {
+  200: Ok,
+  400: BadRequest
+};
+function Ok() {
+  window.location.href = "/WorkSpace/Reports";
+}
+function BadRequest() {
+  swal({ type: "error", title: "Oops...", text: "Bad credentials!" });
+}
+export default {
+  data() {
+    return {
+      myLog: "",
+      myPassw: ""
+    };
+  },
+  components: {
+    Login,
+    Password
+  },
+  methods: {
+    connect() {
+      const xhr = request(
+        "POST",
+        `${this.$root.URL}/auth`,
+        `Login=${this.myLog}&Password=${this.myPassw}`
+      );
+      xhr.onload = function() {
+        console.log(xhr.status);
+        map[xhr.status]();
+      };
+    }
+  }
+};
 </script>
 
 <style src="../../styles/auth.css"></style>
